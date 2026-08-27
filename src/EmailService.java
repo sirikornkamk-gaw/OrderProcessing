d792b0804aee4ec22133684e80d4fc3e9cad5be5
sirikornkamk-gaw <sirikorn.kamk@ku.th>
2026-08-27 17:12:26 +0700

    Add files via upload

--- src/EmailService.java
+++ src/EmailService.java
@@ -2,7 +2,6 @@
 public class EmailService implements OrderObserver {
     @Override public void update(Order order) {
         // TODO (5b): พิมพ์ "Confirmation email sent to <customerEmail>"
-        //   hint: System.out.println("Confirmation email sent to " + order.customerEmail());
-        /* ====== fill in 1 line here ====== */
+        System.out.println("Confirmation email sent to " + order.customerEmail());
     }
 }
